f4f52c7b5939e0edc4c5ddf28c3e301b252a5bbf
Chris <christopherhpena02@gmail.com>
2025-10-21 22:12:56 -0400

    dot moves automatically wowie

--- script.js
+++ script.js
@@ -11,39 +11,58 @@ x = 200
 y = 200
 ctx.fillRect(x, y, 15, 15)   // draw the square
 
+lastKeyPressed = ''
 function moveDot(){
     console.log('entered movedot')
-    y -= 20
+    if(lastKeyPressed == 'w'){
+        y-= 10
+    }
+    else if(lastKeyPressed == 's'){
+        y+= 10
+    }
+    else if(lastKeyPressed == 'a'){
+        x-= 10
+    }
+    else if(lastKeyPressed == 'd'){
+        x+= 10
+    }
+    else {
+        y-= 10
+    }
+    
+
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillRect (x,y,15,15)
 };
 
-setInterval(moveDot, 1000)
+setInterval(moveDot, 125)
 
 document.addEventListener('keydown', function(event) {
     // event.key contains the key pressed
     
-    if (event.key == 'w'){
+    if (event.key == 'w' && lastKeyPressed != 'w' && lastKeyPressed != 's'){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        y -= 10
         ctx.fillRect (x,y,15,15)
+        lastKeyPressed = 'w'
     }
 
-    if (event.key == 's'){
+    if (event.key == 's' && lastKeyPressed != 's' && lastKeyPressed != 'w'){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        y += 10
         ctx.fillRect (x,y,15,15)
+        lastKeyPressed = 's'
     }
 
-    if (event.key == 'a'){
+    if (event.key == 'a' && lastKeyPressed != 'a' && lastKeyPressed != 'd'){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        x -= 10
         ctx.fillRect (x,y,15,15)
+        lastKeyPressed = 'a'
+
     }
 
-    if (event.key == 'd'){
+    if (event.key == 'd' && lastKeyPressed != 'd' && lastKeyPressed != 'a'){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        x += 10
         ctx.fillRect (x,y,15,15)
+        lastKeyPressed = 'd'
+
     }
 });
