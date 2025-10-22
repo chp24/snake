e29ce5d109e00e673da877e919b94a9e62eda730
Chris <christopherhpena02@gmail.com>
2025-10-21 22:32:42 -0400

    player position resets if border is hit

--- script.js
+++ script.js
@@ -7,62 +7,70 @@ const ctx = canvas.getContext('2d')
 ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 ctx.fillStyle = 'white'       // set the fill color
-x = 200
-y = 200
-ctx.fillRect(x, y, 15, 15)   // draw the square
+head_x = 200
+player_y = 200
+ctx.fillRect(head_x, player_y, 15, 15)   // draw the square
 
 lastKeyPressed = ''
 function moveDot(){
-    console.log('entered movedot')
-    if(lastKeyPressed == 'w'){
-        y-= 10
+    console.log('head_x: ', head_x)
+    console.log('player_y: ', player_y)
+
+    //out of bounds checking
+    if( head_x < 0 || head_x > 400 || player_y < 0 || player_y > 400){
+        //reset player location
+        head_x = 200
+        player_y = 200
+    }
+    //move in last direction pressed
+    else if(lastKeyPressed == 'w'){
+        player_y-= 20
     }
     else if(lastKeyPressed == 's'){
-        y+= 10
+        player_y+= 20
     }
     else if(lastKeyPressed == 'a'){
-        x-= 10
+        head_x-= 20
     }
     else if(lastKeyPressed == 'd'){
-        x+= 10
+        head_x+= 20
     }
     else {
-        y-= 10
+        player_y-= 20
     }
     
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.fillRect (x,y,15,15)
+    ctx.fillRect (head_x,player_y,15,15)
 };
 
-setInterval(moveDot, 125)
+setInterval(moveDot, 180)
 
 document.addEventListener('keydown', function(event) {
-    // event.key contains the key pressed
+    // event.key contains the keypressed
     
-    if (event.key == 'w' && lastKeyPressed != 'w' && lastKeyPressed != 's'){
+    //logic for valid keypresses, basically just dont let player move backwards
+    if (event.key == 'w' && lastKeyPressed != 's'){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.fillRect (x,y,15,15)
+        ctx.fillRect (head_x,player_y,15,15)
         lastKeyPressed = 'w'
     }
 
-    if (event.key == 's' && lastKeyPressed != 's' && lastKeyPressed != 'w'){
+    if (event.key == 's' && lastKeyPressed != 'w'){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.fillRect (x,y,15,15)
+        ctx.fillRect (head_x,player_y,15,15)
         lastKeyPressed = 's'
     }
 
-    if (event.key == 'a' && lastKeyPressed != 'a' && lastKeyPressed != 'd'){
+    if (event.key == 'a' && lastKeyPressed != 'd'){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.fillRect (x,y,15,15)
+        ctx.fillRect (head_x,player_y,15,15)
         lastKeyPressed = 'a'
-
     }
 
-    if (event.key == 'd' && lastKeyPressed != 'd' && lastKeyPressed != 'a'){
+    if (event.key == 'd' && lastKeyPressed != 'a'){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.fillRect (x,y,15,15)
+        ctx.fillRect (head_x,player_y,15,15)
         lastKeyPressed = 'd'
-
     }
 });
